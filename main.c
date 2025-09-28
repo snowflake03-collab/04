7be2d21911e88c92843ac42de18c091464e71ea1
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-09-28 16:02:01 +0900

    04-5

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {   
-    int year;
-    printf("input the year: ");
-    scanf("%d", &year);
+    unsigned int x;
+    int b;
     
-    int Isleap = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+    printf("input a number: ");
+    scanf("%ui", &x);
     
+    for(b=0; x !=0; x >>= 1)
+    {
+             if(x & 1)
+             {
+                  b ++;
+                  } 
+    }
     
+    printf("The result is: %i\n", b);
     
-    printf("Is the year %d the leap year?:", year);
-    printf("%d", Isleap);
     system("PAUSE");
+    return 0;
+    
+      
 }
+
